8ba3c09b445ae1adb2e28f67f8fc8a95def963be
Igor Putina <iputina@mixbook.com>
2018-06-20 18:27:15 +0300

    Implement `add` and `remove` commands

--- 03-command-line-todo-list/todo.rb
+++ 03-command-line-todo-list/todo.rb
@@ -3,40 +3,55 @@
 require "thor"
 require "yaml/store"
 
-store = YAML::Store.new('database.yaml')
+$store = YAML::Store.new('database.yaml')
 
 module Todo
-    class CLI < Thor
-        desc "-a or --add [TASK]", "Adds an item into the list of tasks", :aliases => "-a"
-        map %w[-a --add] => :add
-        def add(task)
-            puts "Adding task: #{task}"
-        end
-
-        desc "-r or --remove [ITEM]", "Removes an item from the list of tasks"
-        map %w[-r --remove] => :remove
-        def remove(task)
-            puts "Removing task #{task}"
-        end
-
-        desc "-l or --list", "List the tasks"
-        map %w[-l --list] => :list
-        def list()
-            puts "Listing tasks"
-        end
-
-        desc "-c or --clear", "Clears all tasks"
-        map %w[-c --clear] => :clear
-        def clear()
-            puts "Clearing tasks"
-        end
-
-        desc "-h or --help", "Displays a list of possible commands"
-        map %w[-h --help] => :help
-        def help
-            super
-        end
+  class CLI < Thor
+    desc "-a or --add [TASK]", "Adds an item into the list of tasks", :aliases => "-a"
+    map %w[-a --add] => :add
+    def add(task)
+      $store.transaction do
+        $store[:tasks] ||= []
+        $store[:tasks] << task
+        $store.commit
+      end
     end
+
+    desc "-r or --remove [ITEM]", "Removes an item from the list of tasks"
+    map %w[-r --remove] => :remove
+    def remove(item)
+      $store.transaction do
+        $store[:tasks] ||= []
+        if $store[:tasks].length < item.to_i
+          shell.say "A task at number #{item} does not exist"
+
+          # Abort this transaction
+          $store.abort
+        end
+
+        $store[:tasks].delete_at(item.to_i - 1)
+        $store.commit
+      end
+    end
+
+    desc "-l or --list", "List the tasks"
+    map %w[-l --list] => :list
+    def list()
+      shell.say "Listing tasks"
+    end
+
+    desc "-c or --clear", "Clears all tasks"
+    map %w[-c --clear] => :clear
+    def clear()
+      puts "Clearing tasks"
+    end
+
+    desc "-h or --help", "Displays a list of possible commands"
+    map %w[-h --help] => :help
+    def help
+      super
+    end
+  end
 end
 
 Todo::CLI.start(ARGV)
